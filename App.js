f50e50abe6c95160146e4d2fec22d936692693fd
Joydeeep Nath <joydeepnath33@gmail.com>
2022-11-02 10:35:48 +0530

    Changed background color to blue

--- App.js
+++ App.js
@@ -1,88 +1,101 @@
-import React, {useEffect, useRef, useState} from "react";
-import { FlatList, View, KeyboardAvoidingView, TextInput, StyleSheet, Text, Platform, TouchableWithoutFeedback, button } from "react-native";
-import ListComponent from "./src/components/ListComponent";
-import InputComponent from "./src/components/InputComponent"
-import Header from "./src/components/Header";
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  FlatList,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  StyleSheet,
+  Text,
+  Platform,
+  TouchableWithoutFeedback,
+  button,
+} from 'react-native';
+import ListComponent from './src/components/ListComponent';
+import InputComponent from './src/components/InputComponent';
+import Header from './src/components/Header';
 import uuid from 'react-native-uuid';
 
-const App = ()=>{
-
-  const [arr, setArr] = useState([{id:1, done: false, task:"hello"}, {id:2, done: true, task:"hello World"}, {id:3, done: true, task:"Sourav"}]);
+const App = () => {
+  const [arr, setArr] = useState([
+    {id: 1, done: false, task: 'hello'},
+    {id: 2, done: true, task: 'hello World'},
+    {id: 3, done: true, task: 'Sourav'},
+  ]);
   const [status, setStatus] = useState(0);
   const prevStatus = useRef();
-   var sendArr = arr;
+  var sendArr = arr;
 
   //  -------------------------------- Set Status ------------------------------
-  const statusType = (now) =>{
+  const statusType = now => {
     prevStatus.current = status;
     setStatus(now);
-  }
+  };
 
   //  -------------------------------- Add Task ------------------------------
 
-  const addTask = (item)=>{
+  const addTask = item => {
     console.log(item);
-    setArr ((pretodo) =>{
+    setArr(pretodo => {
       return [
         {
           id: uuid.v1(),
-          task: item ,
+          task: item,
           done: false,
         },
-        ...pretodo
-      ]
+        ...pretodo,
+      ];
     });
     sendArr = arr;
-  }
+  };
 
-      //  -------------------------------- Task Completed? ------------------------------
-      const completed = (id) => {
-         arr.find((a)=>(a.id === id) && (a.done = !a.done)) 
-         setArr((list) => [...arr]);
+  //  -------------------------------- Task Completed? ------------------------------
+  const completed = id => {
+    arr.find(a => a.id === id && (a.done = !a.done));
+    setArr(list => [...arr]);
 
-         sendArr = arr;
-      }
+    sendArr = arr;
+  };
 
-        //  -------------------------------- Task Deleted? ------------------------------
-        const deleted = (id) => {
-           arr.find((a, index)=> a.id === id && arr.splice(index,1));
-    
-           setArr((list)=>[...arr])
+  //  -------------------------------- Task Deleted? ------------------------------
+  const deleted = id => {
+    arr.find((a, index) => a.id === id && arr.splice(index, 1));
 
-           sendArr = arr
-       }
- 
+    setArr(list => [...arr]);
 
-    //  -------------------------------- return JSX final component ------------------------------
+    sendArr = arr;
+  };
+
+  //  -------------------------------- return JSX final component ------------------------------
 
   return (
-    
     <View style={styles.screen}>
-      <Header statusType = {statusType}/>
-     
-     <View style={styles.body}>
+      <Header statusType={statusType} />
 
-      <ListComponent prevStatus = {prevStatus} status = {status} deleted = {deleted} completed={completed} arr = {sendArr}/>
-      
-      <InputComponent addTask = {addTask}/>
+      <View style={styles.body}>
+        <ListComponent
+          prevStatus={prevStatus}
+          status={status}
+          deleted={deleted}
+          completed={completed}
+          arr={sendArr}
+        />
 
-     </View>
+        <InputComponent addTask={addTask} />
+      </View>
     </View>
-    
-  )
-  }
+  );
+};
 
 const styles = StyleSheet.create({
-  screen:{
+  screen: {
     flex: 1,
-    backgroundColor: "#e8e6e1",
+    backgroundColor: 'blue',
   },
 
-  body:{
+  body: {
     flex: 2,
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
   },
-})
-
+});
 
 export default App;
